a3885c02d20c47143d8cfdfc8a0ae849aef07668
Zannnn-Dev <radityaa8292@gmail.com>
2025-08-28 16:57:07 +0700

    Create index.tsx

---
+++ src/commonsts/commons/layouts/index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../../commons/molecules/Navbar";
+import "./mainLayout.css";
+
+const MainLayout = () => {
+  return (
+    <main className="relative mx-auto box-border flex min-h-screen w-full max-w-[1444px] flex-col gap-4 overflow-x-hidden p-4">
+      <Navbar />
+      <Outlet />
+    </main>
+  );
+};
+
+export default MainLayout;
